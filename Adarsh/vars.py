e8a22985ec259092b80c668932a278b5bfeac193
MrMorningStarX <124602536+MrMorningStarX@users.noreply.github.com>
2023-09-09 13:35:03 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -9,23 +9,23 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv("API_ID", "21407623"))
-    API_HASH = str(getenv("API_HASH", "71526961879d5d662ec9307fe850da90"))
-    BOT_TOKEN = str(getenv("BOT_TOKEN","6325109554:AAFX4GmFhrkO2VhKWzmlX0QexKYB-JRPqAQ"))
+    API_ID = int(getenv("API_ID", "25209363"))
+    API_HASH = str(getenv("API_HASH", "28d9ee3be93a1497b17aac1690dff642"))
+    BOT_TOKEN = str(getenv("BOT_TOKEN","5844621630:AAHkRUG4jq59_54-edOYvH883GvxA6iwHeE"))
     name = str(getenv("name", "File-To-Link"))
     SLEEP_THRESHOLD = int(getenv("SLEEP_THRESHOLD", "60"))
     WORKERS = int(getenv("WORKERS", "4"))
-    BIN_CHANNEL = int(getenv("BIN_CHANNEL", "-1001949163175"))
+    BIN_CHANNEL = int(getenv("BIN_CHANNEL", "-1001960045851"))
     PORT = int(getenv("PORT", 8080))
     BIND_ADRESS = str(getenv("WEB_SERVER_BIND_ADDRESS", "0.0.0.0"))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = {int(x) for x in os.environ.get("OWNER_ID", "5297391576").split()}
+    OWNER_ID = {int(x) for x in os.environ.get("OWNER_ID", "1633033029").split()}
     NO_PORT = bool(getenv("NO_PORT", False))
     APP_NAME = None
     OWNER_USERNAME = str(getenv("OWNER_USERNAME", "chill_yaaaar"))
     if "DYNO" in environ:
         ON_HEROKU = True
-        APP_NAME = str(getenv("APP_NAME", "filestreampromplaylink"))
+        APP_NAME = str(getenv("APP_NAME", "OnlineStreamPro"))
 
     else:
         ON_HEROKU = False
