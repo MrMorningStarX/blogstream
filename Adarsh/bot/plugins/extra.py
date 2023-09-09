f20d53c566e6b56850ce38282ca1452bf189665e
MrMorningStarX <124602536+MrMorningStarX@users.noreply.github.com>
2023-09-09 14:30:42 +0530

    Update extra.py

--- Adarsh/bot/plugins/extra.py
+++ Adarsh/bot/plugins/extra.py
@@ -17,7 +17,7 @@ async def maintainers(b,m):
     except Exception:
                 await b.send_message(
                     chat_id=m.chat.id,
-                    text="I am Coded By [Adarsh Goel](https://github.com/adarsh-goel)",
+                    text="I am Coded By [Meth](https://github.com/)",
                     
                     reply_markup=InlineKeyboardMarkup(
                         [
@@ -42,7 +42,7 @@ async def follow_user(b,m):
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("FOLLOW ME", url=f"https://GITHUB.COM/adarsh-goel")
+                                InlineKeyboardButton("FOLLOW ME", url=f"https://GITHUB.COM/")
                             ]
                         ]
                     ),
